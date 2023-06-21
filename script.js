4aaf702d2764e5df312f0020d35221ba0ff96c41
pranjali24-k <127399894+pranjali24-k@users.noreply.github.com>
2023-06-21 08:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// complete this js code
-function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function() { //method using prototype
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);// prototype of person is inherited into employee
+Employee.prototype.constructor = Employee; //get constructor of employee
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
